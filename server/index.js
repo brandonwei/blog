8b33f52a38a259732504416a61e51c8ecda17a25
Brandon <brandonwei@tencent.com>
2017-03-20 00:33:58 +0800

    更新

--- server/index.js
+++ server/index.js
@@ -1,27 +1,59 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var dao = require('./lib/dao_mongo');
 
-app.get("/mongodb", function(req, res){
-    var clientIp = getClientIp(req)
-    console.log(clientIp);
-    console.log(req);
-    res.send("<h>mongodb</h>");
-    //var data = {"id":"test","content":"test"};
-    //dao.insert("blog_article", data,  function(rs){
-    //    console.log(rs)
-    //});
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post("/add", function(req, res) {
+     var title = req.param("title");
+     var content = req.param("content");
+     var time = Date.now();
+     var data = {
+        "title": title,
+        "time": time,
+        "content": content
+     }
+
+     dao.insert("blog_article", data, function(rs) {
+        console.log(rs);
+        var returnData = {};
+        if (rs.result.ok == 1) {
+            returnData.code = 0;
+            returnData.data = {
+                id: rs.ops[0]._id,
+                title: rs.ops[0].title,
+                content: rs.ops[0].content,
+                time: rs.ops[0].time
+            }
+        }
+        else {
+            returnData.code = -1;
+        }
+        res.set("Access-Control-Allow-Origin", "*");
+        res.send(JSON.stringify(returnData));
+     });
 });
 
-app.post("/", function(req, res){
+app.post("/find", function(req, res){
+    cosole.log(req);
+});
+
+app.post("/delete", function(req, res){
+    cosole.log(req);
+});
+
+app.post("/update", function(req, res){
     cosole.log(req);
 });
 
 var getClientIp = function(req)
 {
-        return req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress;
-    };
+    return req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+};
 app.listen(3000);
