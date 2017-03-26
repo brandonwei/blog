920607e5754943149d694828607dd3909484478e
Brandon <brandonwei@tencent.com>
2017-03-27 00:40:50 +0800

    更新

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var dao = require('./lib/dao_mongo');
+var ObjectID = require('mongodb').ObjectID;
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -11,11 +12,17 @@ app.post("/add", function(req, res) {
      var title = req.param("title");
      var content = req.param("content");
      var time = Date.now();
+     if (!title || !content) {
+        res.send(JSON.stringify({
+            code: -1,
+            message: "参数错误"
+        }))
+     }
      var data = {
         "title": title,
         "time": time,
         "content": content
-     }
+     };
 
      dao.insert("blog_article", data, function(rs) {
         console.log(rs);
@@ -32,17 +39,56 @@ app.post("/add", function(req, res) {
         else {
             returnData.code = -1;
         }
-        res.set("Access-Control-Allow-Origin", "*");
+        res.set("Access-Control-Allow-Origin", "http://blog.brandonwei.cn");
         res.send(JSON.stringify(returnData));
      });
 });
 
-app.post("/find", function(req, res){
-    cosole.log(req);
+app.get("/find", function(req, res){
+    var id = req.query["id"];
+    var page = req.query["page"];
+    var data = {};
+    if (id) {
+        data._id = ObjectID(id);
+    }
+    else if (page > 0) {
+
+    }
+    else {
+
+    }
+    dao.find("blog_article", data, function(rs) {
+        console.log(rs);
+        var returnData = {};
+        returnData.code = 0;
+        returnData.data = rs;
+        res.set("Access-Control-Allow-Origin", "http://blog.brandonwei.cn");
+        res.send(JSON.stringify(returnData));
+    });
 });
 
 app.post("/delete", function(req, res){
-    cosole.log(req);
+    console.log("test delete");
+    var id = req.param("id");
+    var data = {};
+    if (id) {
+        data._id = ObjectID(id);
+    }
+    else {
+        res.send(JSON.stringify({
+            code: -1,
+            message: "参数错误", 
+        }))；
+        return;
+    }
+    dao.remove("blog_article", data, function(rs) {
+        console.log(rs);
+        var returnData = {};
+        returnData.code = 0;
+        returnData.data = rs;
+        res.set("Access-Control-Allow-Origin", "*");
+        res.send(JSON.stringify(returnData));
+    });
 });
 
 app.post("/update", function(req, res){
